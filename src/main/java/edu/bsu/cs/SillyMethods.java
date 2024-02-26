4189c4f92c2adeb3dca31edd33568b0a6d859a14
carra <allison.carr@bsu.edu>
2024-02-26 12:25:58 -0500

    Add 4 unit tests to test the 2 public methods in SillyMethods class.

---
+++ src/main/java/edu/bsu/cs/SillyMethods.java
@@ -0,0 +1,18 @@
+package edu.bsu.cs;
+
+public class SillyMethods {
+    public boolean didGuess42(int guess) {
+        return guess == 42;
+    }
+    public String countTo(int n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i <= n; i++) {
+            result.append(i);
+            if (i < n) {
+                result.append(" ");
+            }
+        }
+        return result.toString();
+    }
+}
+
